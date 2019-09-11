bfd84d0a21c6c0057b3d60ff028d1a9bee092c1d
cuongtruong <trgcuong@gmail.com>
2019-09-11 14:12:07 +0700

    Initial commit

---
+++ app/src/main/java/com/jimulabs/googlemusicmock/AlbumDetailActivity.java
@@ -0,0 +1,268 @@
+package com.jimulabs.googlemusicmock;
+
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.transition.Transition;
+import android.transition.TransitionInflater;
+import android.transition.TransitionSet;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.jimulabs.googlemusicmock.transition.RevealTransition;
+
+import butterknife.BindView;
+import butterknife.BindViews;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+
+public class AlbumDetailActivity extends AppCompatActivity {
+
+    public static final String EXTRA_ALBUM_ART_RESID = "EXTRA_ALBUM_ART_RESID";
+    public static final String EXTRA_EPICENTER = "EXTRA_EPICENTER";
+
+    @BindView(R.id.album_art)
+    ImageView albumArtView;
+    @BindViews({R.id.fab, R.id.title_container, R.id.title, R.id.subtitle, R.id.info_container})
+    View[] viewsToAnimate;
+    @BindView(R.id.fab)
+    ImageButton fab;
+    @BindView(R.id.title_container)
+    ViewGroup titleContainer;
+    @BindView(R.id.info_container)
+    ViewGroup infoContainer;
+    @BindView(R.id.title)
+    TextView title;
+    @BindView(R.id.subtitle)
+    TextView subtitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_album_detail);
+        ButterKnife.bind(this);
+        initTransitions();
+        populate();
+        setViewsVisibility(false);
+    }
+
+    @OnClick(R.id.album_art)
+    public void onAlbumArtClicked() {
+        toggleTransitViews(null);
+    }
+
+    private void toggleTransitViews(Transition.TransitionListener listener) {
+        final ViewGroup root = (ViewGroup) getWindow().getDecorView();
+        TransitionInflater inflater = TransitionInflater.from(this);
+        boolean toShow = viewsToAnimate[0].getVisibility() == View.INVISIBLE;
+        Transition transition = inflater.inflateTransition(
+                toShow ? R.transition.album_detail_enter : R.transition.album_detail_return);
+        if (listener!=null) {
+            transition.addListener(listener);
+        }
+        getContentTransitionManager().beginDelayedTransition(root, transition);
+        setViewsVisibility(toShow);
+    }
+
+    private void setViewsVisibility(boolean toShow) {
+        for (View v : viewsToAnimate) {
+            v.setVisibility(toShow ? View.VISIBLE : View.INVISIBLE);
+        }
+        float scale = toShow ? 1 : 0;
+        fab.setScaleX(scale);
+        fab.setScaleY(scale);
+    }
+
+    private void initTransitions() {
+        TransitionInflater inflater = TransitionInflater.from(this);
+        Window window = getWindow();
+        RevealTransition reveal = createRevealTransition();
+        reveal.addListener(new Transition.TransitionListener() {
+            @Override
+            public void onTransitionStart(Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionEnd(Transition transition) {
+                playOtherEnterAnimations();
+            }
+
+            @Override
+            public void onTransitionCancel(Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionPause(Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionResume(Transition transition) {
+
+            }
+        });
+        window.setEnterTransition(reveal);
+    }
+
+    class SetVisibleAnimatorListener implements Animator.AnimatorListener {
+        private final View target;
+
+        SetVisibleAnimatorListener(View target) {
+            this.target = target;
+        }
+        @Override
+        public void onAnimationStart(Animator animation) {
+            target.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void onAnimationEnd(Animator animation) {
+        }
+
+        @Override
+        public void onAnimationCancel(Animator animation) {
+        }
+
+        @Override
+        public void onAnimationRepeat(Animator animation) {
+        }
+    };
+
+    private void playOtherEnterAnimations() {
+        Animator scaleX = ObjectAnimator.ofFloat(fab, "scaleX", 0, 1);
+        Animator scaleY = ObjectAnimator.ofFloat(fab, "scaleY", 0, 1);
+        Animator scaleFab = together(scaleX, scaleY);
+        scaleFab.setTarget(fab);
+        scaleFab.addListener(new SetVisibleAnimatorListener(fab));
+
+        Animator unfoldTitleContainer = createUnfoldAnimator(titleContainer);
+        Animator unfoldInfoContainer = createUnfoldAnimator(infoContainer);
+
+        Animator fadeInTitle = createFadeInAnimator(title);
+        Animator fadeInSubtitle = createFadeInAnimator(subtitle);
+
+        Animator transition = together(sequence(together(unfoldTitleContainer, fadeInTitle, fadeInSubtitle),
+                unfoldInfoContainer), scaleFab);
+
+        transition.start();
+    }
+
+    private Animator createFadeInAnimator(View view) {
+        Animator animator = ObjectAnimator.ofFloat(view, "alpha", 0, 1);
+        animator.addListener(new SetVisibleAnimatorListener(view));
+        return animator;
+    }
+
+    private AnimatorSet sequence(Animator... animators) {
+        AnimatorSet set = new AnimatorSet();
+        set.playSequentially(animators);
+        return set;
+    }
+
+    private Animator createUnfoldAnimator(ViewGroup view) {
+        ObjectAnimator animator = ObjectAnimator.ofInt(view, "bottom", view.getTop(),
+                view.getTop() + view.getHeight());
+        animator.addListener(new SetVisibleAnimatorListener(view));
+        return animator;
+    }
+
+    private Animator together(Animator... animators) {
+        AnimatorSet set = new AnimatorSet();
+        set.playTogether(animators);
+        return set;
+    }
+
+    @Override
+    public void finishAfterTransition() {
+        toggleTransitViews(new Transition.TransitionListener() {
+            @Override
+            public void onTransitionStart(Transition transition) {
+            }
+
+            @Override
+            public void onTransitionEnd(Transition transition) {
+                superFinishAfterTransition();
+            }
+
+            @Override
+            public void onTransitionCancel(Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionPause(Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionResume(Transition transition) {
+
+            }
+        });
+    }
+
+    private void superFinishAfterTransition() {
+        super.finishAfterTransition();
+    }
+
+    private TransitionSet sequence(Transition... transitions) {
+        TransitionSet enterTransition = new TransitionSet();
+        enterTransition.setOrdering(TransitionSet.ORDERING_SEQUENTIAL);
+        for (Transition t : transitions) {
+            enterTransition.addTransition(t);
+        }
+        return enterTransition;
+    }
+
+    private RevealTransition createRevealTransition() {
+        Point epicenter = getIntent().getParcelableExtra(EXTRA_EPICENTER);
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        int bigRadius = Math.max(displayMetrics.widthPixels, displayMetrics.heightPixels);
+        RevealTransition reveal = new RevealTransition(epicenter, 0, bigRadius, 500);
+        reveal.addTarget(R.id.detail_container);
+        reveal.addTarget(android.R.id.statusBarBackground);
+        return reveal;
+    }
+
+    private void populate() {
+        int albumArtResId = getIntent().getIntExtra(EXTRA_ALBUM_ART_RESID, 0);
+        albumArtView.setImageResource(albumArtResId);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_album_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ app/src/main/java/com/jimulabs/googlemusicmock/AlbumListActivity.java
@@ -0,0 +1,132 @@
+package com.jimulabs.googlemusicmock;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class AlbumListActivity extends AppCompatActivity {
+
+    @BindView(R.id.album_list)
+    RecyclerView mAlbumList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_album_list);
+        initTransitions();
+
+        ButterKnife.bind(this);
+        populate();
+    }
+
+    private void initTransitions() {
+        getWindow().setExitTransition(null);
+        getWindow().setReenterTransition(null);
+    }
+
+    interface OnVHClickedListener {
+        void onVHClicked(AlbumVH vh);
+    }
+
+    static class AlbumVH extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private final OnVHClickedListener mListener;
+        @BindView(R.id.album_art)
+        ImageView albumArt;
+
+        public AlbumVH(View itemView, OnVHClickedListener listener) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            itemView.setOnClickListener(this);
+            mListener = listener;
+        }
+
+        @Override
+        public void onClick(View v) {
+            mListener.onVHClicked(this);
+        }
+    }
+
+    private void populate() {
+        StaggeredGridLayoutManager lm = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        mAlbumList.setLayoutManager(lm);
+        final int[] albumArts = {R.drawable.christina,
+                R.drawable.ellie,
+                R.drawable.foster,
+                R.drawable.keane,
+                R.drawable.kodaline,
+                R.drawable.pinkrobots,};
+        RecyclerView.Adapter adapter = new RecyclerView.Adapter<AlbumVH>() {
+            @Override
+            public AlbumVH onCreateViewHolder(ViewGroup parent, int viewType) {
+                View albumView = getLayoutInflater().inflate(R.layout.album_grid_item, parent, false);
+                return new AlbumVH(albumView, new OnVHClickedListener() {
+                    @Override
+                    public void onVHClicked(AlbumVH vh) {
+                        int albumArtResId = albumArts[vh.getPosition() % albumArts.length];
+                        Intent intent = new Intent(AlbumListActivity.this, AlbumDetailActivity.class);
+                        intent.putExtra(AlbumDetailActivity.EXTRA_ALBUM_ART_RESID, albumArtResId);
+
+                        int[] location = new int[2];
+                        vh.albumArt.getLocationInWindow(location);
+                        Point epicenter = new Point(location[0] + vh.albumArt.getMeasuredWidth() / 2,
+                                location[1] + vh.albumArt.getMeasuredHeight() / 2);
+                        intent.putExtra(AlbumDetailActivity.EXTRA_EPICENTER, epicenter);
+
+                        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(AlbumListActivity.this,
+                                vh.albumArt, vh.albumArt.getTransitionName());
+                        startActivity(intent, options.toBundle());
+                    }
+                });
+            }
+
+            @Override
+            public void onBindViewHolder(AlbumVH holder, int position) {
+                holder.albumArt.setImageResource(albumArts[position % albumArts.length]);
+            }
+
+            @Override
+            public int getItemCount() {
+                return albumArts.length * 4;
+            }
+
+        };
+        mAlbumList.setAdapter(adapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_album_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
